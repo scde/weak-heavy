7f6b9eadcf5db3ca743d67c2b3188a0a841ce22f
scde <sd888041@gmail.com>
2017-06-18 21:57:52 +0200

    Add base for platformer (tiny world, player, jump/run, camera)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float m_DampTime = 0.2f;
+    public float m_ScreenEdgeBuffer = 4f;
+    public float m_MinSize = 6.5f;
+    //public float m_MaxSize = ?f; // If implemented TODO block player movement.
+    [HideInInspector] public Transform[] m_Targets; // show in Inspector to add Targets with drag and drop
+
+    private Camera m_Camera;
+    private float m_ZoomSpeed;
+    private Vector3 m_MoveVelocity;
+    private Vector3 m_DesiredPosition;
+
+    // Use this for initialization
+    private void Start () {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+    private void FixedUpdate() {
+        Move();
+        Zoom();
+    }
+
+    // Update is called once per frame
+    private void Update () {
+    }
+
+    private void Move() {
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+    private void FindAveragePosition() {
+        Vector3 averagePos = new Vector3();
+
+        for (int i = 0; i < m_Targets.Length; i++)
+            averagePos += m_Targets[i].position;
+
+        averagePos /= m_Targets.Length;
+
+        // Fix(ate) z position (because of 2D)
+        averagePos.z = transform.position.z;
+
+        m_DesiredPosition = averagePos;
+    }
+
+    private void Zoom() {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+    private float FindRequiredSize() {
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++) {
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+        }
+
+        size += m_ScreenEdgeBuffer;
+
+        size = Mathf.Max(size, m_MinSize);
+
+        //size = Mathf.Min(size, m_MaxSize);
+
+        return size;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int player = 0;
+    public float maxSpeed = 10f;
+    public float jumpForce = 500f;
+    public bool canDoubleJump = false;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private bool grounded = false;
+    private float groundRadius = 0.2f;
+    private bool doubleJump = false;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        if (grounded) {
+            doubleJump = false;
+        }
+
+        float move = Input.GetAxis("Horizontal_P" + (player + 1));
+
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+        //rb2d.AddForce(new Vector2(move * maxSpeed, rb2d.velocity.y));
+
+        // Flips character (sprites) into moving direction
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if ((grounded || (!doubleJump && canDoubleJump))  && Input.GetButtonDown("Jump_P" + (player + 1))) {
+            //grounded = false;
+            rb2d.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded) {
+                doubleJump = true;
+            }
+        }
+    }
+
+    void Flip () {
+        facingRight = !facingRight;
+        Vector3 currentLocalScale = transform.localScale;
+
+        currentLocalScale.x *= -1;
+        transform.localScale = currentLocalScale;
+    }
+}
