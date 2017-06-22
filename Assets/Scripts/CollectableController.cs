6e85079ca4e0db6184fc576541dd97b72ff35528
scde <sd888041@gmail.com>
2017-06-22 16:53:44 +0200

    Adds buggy walljump and coin

---
+++ Assets/Scripts/CollectableController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player")
+        {
+            //if (gameObject.activeSelf) // use this when you want to temporary disable a gameobject
+            //{
+                //gameObject.SetActive(false);
+            //}
+            // TODO: add item to inventory
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,38 @@ public class PlayerController : MonoBehaviour {
     public int player = 0;
     public float maxSpeed = 10f;
     public float jumpForce = 500f;
+    public float horizontalWallJumpForce = 1000f;
     public bool canDoubleJump = false;
+    public bool canWallJump = false;
     public Transform groundCheck;
+    public Transform wallCheck;
     public LayerMask whatIsGround;
+    public LayerMask whatIsWall;
 
     private Rigidbody2D rb2d;
     private bool facingRight = true;
     private bool grounded = false;
+    private bool onWall = false;
     private float groundRadius = 0.2f;
+    private float wallRadius = 0.5f;
     private bool doubleJump = false;
+
+
 
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
+	// FixedUpdate is called once per pyhsics timestep
     void FixedUpdate () {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        onWall = Physics2D.OverlapCircle(wallCheck.position, wallRadius, whatIsWall);
         if (grounded) {
+            doubleJump = false;
+        }
+        if (onWall) {
+            grounded = false;
             doubleJump = false;
         }
 
@@ -42,14 +56,31 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if ((grounded || (!doubleJump && canDoubleJump))  && Input.GetButtonDown("Jump_P" + (player + 1))) {
-            //grounded = false;
+        if ((grounded || (!doubleJump && canDoubleJump)) && Input.GetButtonDown("Jump_P" + (player + 1))) {
+			if (!grounded) {
+				print ("Doublejump!");
+			} else {
+				print ("Jump!");
+			}
             rb2d.AddForce(new Vector2(0, jumpForce));
 
             if (!doubleJump && !grounded) {
                 doubleJump = true;
             }
         }
+        if (canWallJump && onWall && Input.GetButtonDown("Jump_P" + (player + 1))) {
+			print ("Walljump!");
+            WallJump();
+        }
+    }
+
+    private void WallJump() {
+        //rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+        rb2d.AddForce(new Vector2(0,jumpForce));
+        //if (facingRight)
+            //rb2d.velocity = new Vector2(maxSpeed * -1, rb2d.velocity.y);
+        //else
+            //rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
     }
 
     void Flip () {
