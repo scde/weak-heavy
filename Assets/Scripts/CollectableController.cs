f7d999dde3b5e8e3440166fb444842b2c48e86b8
scde <sd888041@gmail.com>
2017-06-27 15:40:16 +0200

    Add basic GUI with coin counter

--- Assets/Scripts/CollectableController.cs
+++ Assets/Scripts/CollectableController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CollectableController : MonoBehaviour {
 
+    public GameController gameController;
+
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.tag == "Player")
         {
             //if (gameObject.activeSelf) // use this when you want to temporary disable a gameobject
             //{
-                //gameObject.SetActive(false);
+            //gameObject.SetActive(false);
             //}
             // TODO: add item to inventory
+            gameController.AddCoin();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text coinText;
+
+	private int coinCounter;
+
+	private void Start() {
+        coinCounter = 0;
+        UpdateCoinText();
+    }
+
+    public void AddCoin() {
+        coinCounter++;
+        UpdateCoinText();
+    }
+
+    private void UpdateCoinText() {
+        coinText.text = "Coin:\n" + coinCounter;
+    }
+}
