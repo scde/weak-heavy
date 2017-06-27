cda11aeac39b1dcfe9abb941346f7794a0d307a1
Thilo Ziel <thilo.ziel@outlook.de>
2017-06-27 12:37:06 +0200

    - first implementations for Heavy - structure for PlayerController

---
+++ Assets/Scripts/Arrow/fireArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float timeBetweenBullets = 0.15f;
+	public GameObject projectile;
+
+	float nextBullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake () {
+		nextBullet = 0f;
+	}
+}
