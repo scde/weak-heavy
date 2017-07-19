d85a2ac61ea9e23dbb5d0b01046388b8b95be41e
scde <sd888041@gmail.com>
2017-07-19 15:54:28 +0200

    Add static Utilities class with helper functions
    For now it only holds a conversion function from jump height to initial
    jump velocity but it might be needed later with other functions too.

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities {
+    public static float InitialJumpVelocity(float jumpHeight)
+    {
+		// source: https://math.stackexchange.com/questions/785375/calculate-initial-velocity-to-reach-height-y#785397
+		return Mathf.Sqrt(2 * Mathf.Abs(Physics2D.gravity.y) * jumpHeight);
+    }
+}
