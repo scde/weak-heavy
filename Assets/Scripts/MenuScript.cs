73d28f760879ebfa07c3607ad6aca78b894887fa
Thilo Ziel <thiloziel@dhcp200005.uni-regensburg.de>
2017-08-11 12:37:00 +0200

    - implemented Pause function and created Pause Screen - implemented and created StartScreen

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public void ChangeScene(string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+
+	public void OnCLick(){
+		Debug.Log ("Click detected");
+	}
+
+	public void Finish(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MenuScript {
+
+	public GameObject pausePanel;
+
+	public bool isPaused;
+
+
+	void Start(){
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) {
+			PauseGame (true);
+		} else {
+			PauseGame (false);
+		}
+
+		if (Input.GetButtonDown ("Cancel")) {
+			SwitchPaused ();
+		}
+	}
+
+	void PauseGame(bool state){
+		if (state) {
+			pausePanel.SetActive (true);
+			Time.timeScale = 0.0f;
+		} else {
+			pausePanel.SetActive (false);
+			Time.timeScale = 1.0f;
+		}
+	}
+
+	public void SwitchPaused(){
+		if (isPaused) {
+			isPaused = false;
+		} else {
+			isPaused = true;
+		}
+	}
+}
