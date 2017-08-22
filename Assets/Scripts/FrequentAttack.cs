1eb85438c86327ae70d1b2684edd2ef266d3ac5f
Thilo Ziel <thiloziel@thilos-mbp.localdomain>
2017-08-22 22:43:38 +0200

    implemented Frequent attack for Enemy Scissor updated Enemy Scissor Prefab

---
+++ Assets/Scripts/FrequentAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrequentAttack : MonoBehaviour {
+
+	public float attackCooldown;
+
+	private float nextAttackTime = 0.0f;
+
+	private AttackController attackController;
+
+	// Use this for initialization
+	void Start () {
+		attackController = GetComponentInChildren<AttackController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nextAttackTime +=  Time.deltaTime;
+
+		if (nextAttackTime >= attackCooldown) {
+			nextAttackTime = nextAttackTime - attackCooldown;
+
+			attackController.Attack ();
+			Debug.Log ("Scissor Attacks");
+		}
+
+	}
+}
