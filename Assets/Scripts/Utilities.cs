005d77cdedcc54e0a7af755943e7a3711b00cab6
scde <sd888041@gmail.com>
2017-09-06 02:37:20 +0200

    Add new utilities function to get clip length
    Finds the clip length of a animation given a animator and the exact
    animation name.

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Utilities {
+public static class Utilities
+{
     // LayerMask check (source): http://answers.unity3d.com/answers/454913/view.html
     public static bool CheckLayerMask(LayerMask mask, GameObject obj)
     {
-        return (mask.value & 1<<obj.layer) != 0;
+        return (mask.value & 1 << obj.layer) != 0;
     }
 
     // source: http://answers.unity3d.com/questions/823090/equivalent-of-degree-to-vector2-in-unity.html
@@ -25,9 +26,25 @@ public static class Utilities {
         return Mathf.Sqrt(2 * Mathf.Abs(Physics2D.gravity.y) * jumpHeight);
     }
 
-	public static IEnumerator waitForRespawn(float RespawnTime, GameObject gOBJ){
-		gOBJ.SetActive (false);
-		yield return new WaitForSeconds(RespawnTime);
-		gOBJ.SetActive (true);
-	}
+    public static IEnumerator waitForRespawn(float RespawnTime, GameObject gOBJ)
+    {
+        gOBJ.SetActive(false);
+        yield return new WaitForSeconds(RespawnTime);
+        gOBJ.SetActive(true);
+    }
+
+    public static float GetClipLength(Animator anim, string clipName)
+    {
+        float clipLength = 0.0f;
+        RuntimeAnimatorController ac = anim.runtimeAnimatorController;
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if (ac.animationClips[i].name == clipName)
+            {
+                clipLength = ac.animationClips[i].length;
+                break;
+            }
+        }
+        return clipLength;
+    }
 }
